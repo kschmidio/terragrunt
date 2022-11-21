83d74537037099a19fef60ab93be04ff9cb2c73c
Karl Schmid <net.karl.schmid@mhp.com>
2022-11-21 21:43:34 +0100

    adjusted source path

--- product2/main.tf
+++ product2/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 module "create_resource_group" {
-	source = "../../modules/resourcegroup"
+	source = "../modules/resourcegroup"
 	resource_group_name = var.resource_group_name
 	location = var.location
 }
